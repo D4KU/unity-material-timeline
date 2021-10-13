8048b1abab1d7401792ec1b188ed4dc7aac5524e
D4KU <d4k@tuta.io>
2021-10-14 00:23:58 +0200

    Made clips show meaningful display name, changed track color

--- Runtime/MaterialTrack.cs
+++ Runtime/MaterialTrack.cs
@@ -3,11 +3,19 @@ using UnityEngine.Playables;
 using UnityEngine.Timeline;
 
 [TrackBindingType(typeof(Material))]
-[TrackColor(1, 0, 0)]
+[TrackColor(.5f, .84f, 1f)]
 [TrackClipType(typeof(MaterialClip))]
 public class MaterialTrack : TrackAsset
 {
     public override Playable CreateTrackMixer(
             PlayableGraph graph, GameObject go, int inputCount)
-        => ScriptPlayable<MaterialMixer>.Create(graph, inputCount);
+    {
+        // Set display name of each clip
+        foreach (TimelineClip clip in GetClips())
+        {
+            MaterialBehaviour data = ((MaterialClip)clip.asset).data;
+            clip.displayName = $"{data.propertyName} [{data.propertyType}]";
+        }
+        return ScriptPlayable<MaterialMixer>.Create(graph, inputCount);
+    }
 }
